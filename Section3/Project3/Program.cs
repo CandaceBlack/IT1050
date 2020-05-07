f9279af17748f844ccf451b708eb9e9f2ed36739
CandaceBlack <59985409+CandaceBlack@users.noreply.github.com>
2020-05-07 15:34:35 -0400

    Committing Project3, will fix

---
+++ Section3/Project3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instructors
+
+            Instructor John = new Instructor("John", "English");
+            Instructor Mike = new Instructor("Mike", "Math");
+
+            //Students
+            Student Jane = new Student("Jane", John);
+            Student Joe = new Student("Joe", John);
+
+            Student Melissa = new Student("Melissa", Mike);
+            Student Matt = new Student("Matt", Mike);
+
+            //Set Grade
+            John.SetStudentGrade(Jane, 95);
+            John.SetStudentGrade(Joe, 85);
+
+            Mike.SetStudentGrade(Melissa, 90);
+            Mike.SetStudentGrade(Matt, 92);
+
+            //Print
+            Jane.Print();
+            Joe.Print();
+
+            Melissa.Print();
+            Matt.Print();
+
+            Console.WriteLine("Press any key to continue... ");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Section3/Project3/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3
+{
+    class Student
+    {
+        private string Name;
+        private int Grade;
+        private Instructor Teacher;
+
+        public Student(string name, Instructor teacher)
+        {
+            this.Name = name;
+            this.Teacher = teacher;
+            this.Grade = 0;
+        }
+
+        public void SetGrade(int grade)
+        {
+            this.Grade = 0;
+            this.Grade += grade;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Name" + this.Name + " has a " + this.Grade + "from" + this.Teacher + "." );
+            
+            Teacher.Print();
+        }
+
+    }
+}
