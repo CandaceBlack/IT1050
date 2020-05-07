49585b76c650156daa5e929736457c90e829a1d2
CandaceBlack <59985409+CandaceBlack@users.noreply.github.com>
2020-05-07 01:40:13 -0400

    Deleted index file and re added project 2 to new subfolder section 2

---
+++ Section2/Project2/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+
+        public static double SumOfAllAges;
+
+        public string GetFullName()
+        {
+            return this.LastName + ", " + this.FirstName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(this.GetFullName() + " (" + this.Age + ")");
+        }
+
+        public void GetInfo()
+        {
+            System.Console.WriteLine("What is your first name? ");
+            this.FirstName = System.Console.ReadLine();
+
+            System.Console.WriteLine("What is your last name? ");
+            this.LastName = System.Console.ReadLine();
+
+            System.Console.WriteLine("How old are you? ");
+            this.Age = int.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("What is your spouse's first name? ");
+            this.Spouse = new Person();
+            this.Spouse.FirstName = System.Console.ReadLine();
+            this.Spouse.LastName = this.LastName;
+
+            System.Console.WriteLine("How old is your spouse? ");
+            this.Spouse.Age = int.Parse(System.Console.ReadLine());
+
+
+        }
+
+    }
+}
---
+++ Section2/Project2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.GetInfo();
+            Console.WriteLine();
+
+            Person p2 = new Person();
+            p2.GetInfo();
+            Console.WriteLine();
+
+            Console.WriteLine();
+            p1.PrintNameAndAge();
+            p1.Spouse.PrintNameAndAge();
+            p2.PrintNameAndAge();
+            p2.Spouse.PrintNameAndAge();
+
+            Person.SumOfAllAges = (p1.Age + p1.Spouse.Age + p2.Age + p2.Spouse.Age);
+            int numberOfPeople = 4;
+
+            Console.WriteLine("Average Age: " + Person.SumOfAllAges / numberOfPeople);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
