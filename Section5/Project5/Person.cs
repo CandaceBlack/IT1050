b125774823050356f12e27ccfac3a69f00e8d80e
CandaceBlack <59985409+CandaceBlack@users.noreply.github.com>
2020-05-07 23:44:48 -0400

    Deleted index file and added section 5

---
+++ Section5/Project5/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        private string FullName;
+
+
+        public Person(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.FullName = this.FirstName + " " + this.LastName;
+        }
+        public string Print()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+    }
+
+}
---
+++ Section5/Project5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] People = new Person[Count];
+
+
+        }
+
+    }
+}
