291d9f31d0015ff2f90ec14193084c7262fd56b2
CandaceBlack <59985409+CandaceBlack@users.noreply.github.com>
2020-05-10 15:14:11 -0400

    Added a few Writelines to make project look better

--- Section1/MyFirstProject/Program.cs
+++ Section1/MyFirstProject/Program.cs
@@ -14,13 +14,14 @@ namespace MyFirstProject
             System.Console.Write("What is your first name? ");
             string firstName = System.Console.ReadLine();
 
+
             System.Console.Write("What is your middle initial? ");
             string middleInitial = System.Console.ReadLine();
 
-            System.Console.Write("What is your Last name? ");
+            System.Console.Write("What is your last name? ");
             string lastName = System.Console.ReadLine();
 
-            string fullName = firstName + "" + middleInitial + "." + lastName;
+            string fullName = firstName + " " + middleInitial + ". " + lastName;
 
 
             System.Console.Write("How tall are you in feet? ");
@@ -38,14 +39,19 @@ namespace MyFirstProject
             System.Console.Write("How old are you? ");
             int age = int.Parse(Console.ReadLine());
 
-            System.Console.Write("Are you a US Citizen? ");
+            System.Console.Write("Are you a US Citizen?: (True/False) ");
             bool isCitizen = bool.Parse(Console.ReadLine());
 
             bool canVote = (age >= 18) && isCitizen;
 
+            System.Console.WriteLine();
             System.Console.WriteLine("Your full name is: '" + fullName + "'.");
+            System.Console.WriteLine();
             System.Console.WriteLine("Your height in CM is: '" + totalHeightCM + "'.");
+            System.Console.WriteLine();
             System.Console.WriteLine("Your voter eligibility is: '" + canVote + "'.");
+            System.Console.WriteLine();
+            System.Console.WriteLine("Press any key to continue...");
             System.Console.ReadKey();
 
 
